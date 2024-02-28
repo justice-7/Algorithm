4ecf5515a520068f2ed3a6d89024cc4fef7dd46c
justice-7 <81703684+justice-7@users.noreply.github.com>
2024-02-28 10:45:30 +0900

    [Silver III] Title: 1로 만들기, Time: 156 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,22 +1,49 @@
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] dp = new int[n+1];
-		dp[1] = 0;
-		for (int i=2; i<=n; i++) {
-			dp[i] = dp[i-1]+1;
-			//2, 3 모두로 나누어 떨이지는 경우, if-if로 계산해서 더 적은 경우 선택
-			if (i%2==0)
-				dp[i] = Math.min(dp[i], dp[i/2]+1);
-			if (i%3==0)
-				dp[i] = Math.min(dp[i], dp[i/3]+1);
-		}
-		System.out.println(dp[n]);
-		sc.close();
-	}
+        int n = Integer.parseInt(br.readLine());
 
+        if(n==1){
+            System.out.println(0);
+            System.exit(0);
+        }
+        if(n==2){
+            System.out.println(1);
+            System.exit(0);
+        }
+
+
+        int[] dp=new int[n+1];
+
+        Arrays.fill(dp,Integer.MAX_VALUE);
+
+        dp[1]=0;
+        dp[2]=1;
+        dp[3]=1;
+
+        for(int i=4; i<n+1; i++){
+            if(i%3==0){
+                dp[i]=Math.min(dp[i/3]+1,dp[i]);
+            }
+            if(i%2==0){
+                dp[i]=Math.min(dp[i/2]+1,dp[i]);
+            }
+            dp[i]=Math.min(dp[i],dp[i-1]+1);
+
+        }
+//        System.out.println(Arrays.toString(dp));
+
+        System.out.println(dp[n]);
+
+
+    }
 }
