8fd230bb54d37f98da225d552fd94a65f9579012
justice-7 <81703684+justice-7@users.noreply.github.com>
2024-08-05 00:33:29 +0900

    [Gold V] Title: ABCDE, Time: 312 ms, Memory: 18180 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,66 +1,49 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
-/**
- * @author justice_va
- * @date 24.02.21
- * @link https://www.acmicpc.net/problem/13023
- * @keyword_solution 그래프(dfs)
- * @input 
- * 사람 수: 5<= N <=2,000
- * 친구 관계 수: 1<= M <= 2,000
- * @output A,B,C,D,E 존재하면 1, 아니면 0
- * @time_complex O(NM)
- * @perf 
- */
+
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static int n,m, result;
-	static int[] visit,count;
-	static List<List<Integer>> g = new ArrayList<>();
-	public static void main(String[] args) throws IOException {
+	static int n,m,a,b,ok;
+	static List<List<Integer>> g;
+	static int[] visit;
+	public static void main(String[] args) throws Exception{
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-
+		g = new ArrayList<>();
 		for (int i=0; i<n; i++) {
 			g.add(new ArrayList<>());
 		}
+		visit = new int[n];
+		m = Integer.parseInt(st.nextToken());
 		for (int i=0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
 			g.get(a).add(b);
 			g.get(b).add(a);
 		}
-		count = new int[n];
-		result=0;
+		ok = 0;
 		for (int j=0; j<n; j++) {
-			visit = new int[n];
 			visit[j]=1;
-			dfs(j,1); 
-			if (result == 1)
-				break;
+			dfs(j,0);
+			visit[j]=0;
 		}
-		System.out.println(result);
+		System.out.println(ok);
 	}
-
-	public static void dfs(int x,int cnt) {
-		if (result == 1)
-			return;
-		if (cnt==5) {
-			result = 1;
+	public static void dfs(int node, int cnt) {
+		if (cnt==4 || ok==1) {
+			ok=1;
 			return;
 		}
-		for (int i=0; i<g.get(x).size(); i++) {
-			if (visit[g.get(x).get(i)]==0) {
-				visit[g.get(x).get(i)]=1;
-				dfs(g.get(x).get(i),cnt+1);
-				visit[g.get(x).get(i)]=0;
+		for (int i=0; i<g.get(node).size(); i++) {
+			if (visit[g.get(node).get(i)]==0) {
+				visit[g.get(node).get(i)] = 1;
+				dfs(g.get(node).get(i), cnt+1);
+				visit[g.get(node).get(i)] = 0;
 			}
 		}
 	}
