b28eaeb5e9f3c200bc6f02c7d6bf4f71b7023753
justice-7 <81703684+justice-7@users.noreply.github.com>
2024-10-09 17:35:42 +0900

    [Gold V] Title: 두 용액, Time: 340 ms, Memory: 28564 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -6,61 +6,45 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int n;
+	static int[] ar;
 	public static void main(String[] args) throws Exception{
 		n = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
-		List<Integer> minus = new ArrayList<>();
-		List<Integer> plus = new ArrayList<>();
+		ar = new int[n];
  		for (int i=0; i<n; i++) {
-			int a = Integer.parseInt(st.nextToken());
-			if (a>0)
-				plus.add(a);
-			else 
-				minus.add(a);
+			ar[i] = Integer.parseInt(st.nextToken());
 		}
-		Collections.sort(plus);
-		Collections.sort(minus);
-		int mi = minus.size()-1;
-		int pl = 0;
+		Arrays.sort(ar);
 		int left = 0;
-		int right = 0;
+		int right = n-1;
+		int rl = 0; 
+		int rr = 0;
 		int zero = Integer.MAX_VALUE;
-		while(minus.size()>0 && plus.size()>0 && mi>=0 && pl<plus.size()) {
-			int tmp = minus.get(mi)+plus.get(pl);
+		while(left<right) {
+			int tmp = ar[left]+ar[right];
 			if (tmp == 0) {
-				left = minus.get(mi);
-				right = plus.get(pl);
+				rl = left;
+				rr = right;
 				break;
 			}
 			else if (tmp<0){
 				if (zero>Math.abs(tmp)) {
 					zero = Math.abs(tmp);
-					left = minus.get(mi);
-					right = plus.get(pl);
+					rl = left;
+					rr = right;
 				}
-				pl+=1;
+				left+=1;
 			}
 			else {
 				if (zero>Math.abs(tmp)) {
 					zero = Math.abs(tmp);
-					left = minus.get(mi);
-					right = plus.get(pl);
+					rl = left;
+					rr = right;
 				}
-				mi-=1;
+				right-=1;
 			}
 		}
-	
-		if (plus.size()>=2 && zero>plus.get(0)+plus.get(1)) {
-			zero = plus.get(0)+plus.get(1);
-			left = plus.get(0);
-			right = plus.get(1);
-		}
-		if (minus.size()>=2 && zero>Math.abs(minus.get(minus.size()-1)+minus.get(minus.size()-2))) {
-			zero = minus.get(minus.size()-1)+minus.get(minus.size()-2);
-			left = minus.get(minus.size()-2);
-			right = minus.get(minus.size()-1);
-		}
-		System.out.println(left+" "+right);
+		System.out.println(ar[rl]+" "+ar[rr]);
 	}
 
 }
