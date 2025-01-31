db1a205f38cfb354d5ca6b3bef4f88715292bbbb
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-02-01 02:41:52 +0900

    [Silver II] Title: 베르트랑 공준, Time: 180 ms, Memory: 35824 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+max = 123456*2
+prime = [1]*(max+1)
+prime[0], prime[1] = 0,0
+for i in range(2,int(max**0.5)+1):
+    for j in range(i*i, max+1, i):
+        prime[j]=0
+
+while True:
+    n = int(input())
+    if n==0:
+        break
+    print(sum(prime[n+1:2*n+1]))
