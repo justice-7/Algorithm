31307a1491b8a3fb7441d48810e2dd885218fd56
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-02-01 02:42:14 +0900

    [Silver II] Title: 베르트랑 공준, Time: 132 ms, Memory: 35824 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -5,8 +5,9 @@ max = 123456*2
 prime = [1]*(max+1)
 prime[0], prime[1] = 0,0
 for i in range(2,int(max**0.5)+1):
-    for j in range(i*i, max+1, i):
-        prime[j]=0
+    if prime[i]==1:
+        for j in range(i*i, max+1, i):
+            prime[j]=0
 
 while True:
     n = int(input())
