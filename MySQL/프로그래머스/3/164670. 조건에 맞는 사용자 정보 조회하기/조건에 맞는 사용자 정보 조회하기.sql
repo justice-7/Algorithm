06976591d8d2e0ce73cbbb95eba42848d096c7ce
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-03-08 03:12:49 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,13 @@
+-- 중고 거래 게시물 3건 이상
+-- 사용자 ID, 닉네임, 전체 주소, 전화번호
+-- 전체 주소: 시,도로명,상세 / 전화번호: 하이픈 포함
+-- 회원 ID 내림차순
+SELECT USER_ID, NICKNAME, CONCAT(CITY,' ',STREET_ADDRESS1,' ',STREET_ADDRESS2) AS '전체주소', CONCAT(LEFT(TLNO,3),'-',SUBSTR(TLNO,4,4),'-',RIGHT(TLNO,4)) AS '전화번호'
+FROM USED_GOODS_USER JOIN (
+    SELECT WRITER_ID AS W_ID, COUNT(*)
+    FROM USED_GOODS_BOARD
+    GROUP BY WRITER_ID
+    HAVING COUNT(*)>=3
+    ) AS B
+ON USER_ID = B.W_ID
+ORDER BY USER_ID DESC
