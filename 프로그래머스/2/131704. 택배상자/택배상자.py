9a97d441126ba8638f2bd7c3abd0d7296e4a354f
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-05-01 17:03:35 +0900

    [level 2] Title: 택배상자, Time: 170.99 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,20 +1,13 @@
-from collections import deque
 def solution(order):
-    a = deque(range(1, len(order)+1))
     sub = []
-    answer = 0
-    ind = 0
-    while a or sub:
-        if a and a[0]==order[ind]:
+    ind = 0 
+    for i in range(1, len(order)+1):
+        if i==order[ind]:
             ind+=1
-            answer+=1
-            a.popleft()
-        elif sub and sub[-1]==order[ind]:
-            ind+=1
-            answer+=1
-            sub.pop()
-        elif a:
-            sub.append(a.popleft())
         else:
-            break
-    return answer
+            sub.append(i)
+        while sub and sub[-1]==order[ind]:
+            ind+=1
+            sub.pop()
+        
+    return ind
