783e8a73a3d4402c31607dd0b1df0158dd8317cf
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-05-01 17:07:30 +0900

    [level 2] Title: 택배상자, Time: 228.63 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -2,10 +2,7 @@ def solution(order):
     sub = []
     ind = 0 
     for i in range(1, len(order)+1):
-        if i==order[ind]:
-            ind+=1
-        else:
-            sub.append(i)
+        sub.append(i)
         while sub and sub[-1]==order[ind]:
             ind+=1
             sub.pop()
