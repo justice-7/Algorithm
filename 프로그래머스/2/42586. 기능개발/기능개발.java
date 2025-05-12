dfed01a215d777113feba87edcd2d478334b5ded
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-05-13 00:24:00 +0900

    [level 2] Title: 기능개발, Time: 3.47 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,7 +5,7 @@ class Solution {
         int now = -1;
         int cnt = 0;
         for (int i=0; i<progresses.length; i++){
-            int day = (int)Math.ceil((100.0-progresses[i])/speeds[i]);
+            int day = (int)Math.ceil((100.0-progresses[i])/speeds[i]); // 100.0으로 해야 소수 나눗셈 됨. 100으로 하면 정수라 소수점 날아감.
             if (now == -1){
                 now = day;
             }
