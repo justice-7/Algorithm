5bdaf51f48739ca4748faef06b1891350a53dc3e
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-06-05 14:28:27 +0900

    [level 3] Title: 이중우선순위큐, Time: 118.25 ms, Memory: 22 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -2,15 +2,26 @@ import heapq
 def solution(operations):
     min=[]
     max=[]
-    for op in operations:
+    visit = dict()
+    for ind,op in enumerate(operations):
         command, value = op.split(" ")
         value = int(value)
         if command == 'I':
-            heapq.heappush(min,value)
-            heapq.heappush(max,-value)
-        elif max and value == 1:
-            min.remove(-heapq.heappop(max))
-        elif min and value ==-1:
-            max.remove(-heapq.heappop(min))
-               
-    return [0,0] if not max else [-heapq.heappop(max),heapq.heappop(min)]
+            heapq.heappush(min,(value,ind))
+            heapq.heappush(max,(-value,ind))
+            visit[ind]=1
+        elif value == 1:
+            while max and visit.get(max[0][1])==0:
+                heapq.heappop(max)
+            if max:
+                visit[heapq.heappop(max)[1]]=0
+        elif value ==-1:
+            while min and visit.get(min[0][1])==0:
+                heapq.heappop(min)
+            if min:
+                visit[heapq.heappop(min)[1]]=0
+    while max and visit.get(max[0][1])==0:
+                heapq.heappop(max)
+    while min and visit.get(min[0][1])==0:
+                heapq.heappop(min)
+    return [0,0] if not min or not max else [-heapq.heappop(max)[0], heapq.heappop(min)[0]]
