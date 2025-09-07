9bd81fac5b8aff9e1560ca8062f82fd3a6f81aac
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-09-07 15:35:31 +0900

    [Gold IV] Title: 트리, Time: 4640 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.py
+++ 백준/Gold/4803. 트리/트리.py
@@ -1,36 +1,36 @@
-def dfs(node,parent):
-    global cycle
-    visit[node] = 1
-    for nx in tree[node]:
-        if visit[nx]==0:
-            dfs(nx, node)
-        elif nx != parent:
-            cycle = True
+def find(x):
+    if x != parent[x]:
+        parent[x] = find(parent[x])
+    return parent[x]
 
-
+def union(a,b):
+    a = find(a)
+    b = find(b)
+    if a==b:
+        cycle.add(a) # 사이클
+    elif a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
 tc = 0
 while True:
     tc+=1
     n,m = map(int, input().split())
-    tree = [[] for _ in range(n+1)]
     if n==0 and m==0:
         break
+
+    parent = list(range(n+1))
+    cycle = set()
     for _ in range(m):
         s,e = map(int, input().split())
-        tree[s].append(e)
-        tree[e].append(s)
+        union(s,e)
 
-    visit = [0]*(n+1)
-    cnt = 0
-    for i in range(1, n+1):
-        if visit[i]==0:
-            cycle = False
-            dfs(i,0)
-            if not cycle:
-                cnt+=1
-    if cnt == 0:
+    roots = set(find(i) for i in range(1, n+1))
+    cycle = set(find(i) for i in cycle)
+    cnt = len(roots- cycle)
+    if cnt ==0:
         print(f"Case {tc}: No trees.")
-    elif cnt == 1:
+    elif cnt ==1:
         print(f"Case {tc}: There is one tree.")
     else:
         print(f"Case {tc}: A forest of {cnt} trees.")
