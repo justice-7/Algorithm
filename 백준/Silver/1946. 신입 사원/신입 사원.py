ec0275a68cdd2526e6fe516cc371eab1ef522f91
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-10-31 01:30:58 +0900

    [Silver I] Title: 신입 사원, Time: 2580 ms, Memory: 73424 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -4,12 +4,14 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     ar = [list(map(int,input().split())) for _ in range(n)]
-    ar.sort()
-    mi = ar[0][1]
+    rank = [0]*(n+1)
+    for i in range(n):
+        rank[ar[i][0]] = ar[i][1]
+    mi = float('inf')
     cnt = 0
-    for i in range(1,n):
-        if mi > ar[i][1]:
-            mi = ar[i][1]
+    for j in range(1,n+1):
+        if mi>rank[j]:
+            mi = rank[j]
         else:
             cnt+=1
     print(n-cnt)
