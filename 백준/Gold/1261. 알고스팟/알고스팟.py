fc9b45b717d13e4f2ae708961522a7079bf7dfaf
justice-7 <81703684+justice-7@users.noreply.github.com>
2025-10-31 19:51:45 +0900

    [Gold IV] Title: 알고스팟, Time: 68 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,16 +1,15 @@
-import heapq
+from collections import deque
 dr,dc = [1,-1,0,0],[0,0,1,-1]
 m,n = map(int, input().split())
-a = [list(map(int, input())) for _ in range(n)]
+a = [list(map(int, input().strip())) for _ in range(n)]
 INF = float('inf')
 visit = [[INF]*m for _ in range(n)]
 
 def bfs(r,c):
-    q = []
-    heapq.heappush(q,(r,c))
+    q = deque([(r,c)])
     visit[r][c] = 0
     while q:
-        r,c = heapq.heappop(q)
+        r,c = q.popleft()
         for i in range(4):
             nr = r+dr[i]
             nc = c+dc[i]
@@ -19,10 +18,10 @@ def bfs(r,c):
             if a[nr][nc]==1:
                 if visit[nr][nc] > visit[r][c]+1:
                     visit[nr][nc] = visit[r][c]+1
-                    q.append((nr, nc))
+                    q.append((nr,nc))
             elif visit[nr][nc] > visit[r][c]:
                 visit[nr][nc] = visit[r][c]
-                q.append((nr, nc))
+                q.appendleft((nr,nc))
 
 bfs(0,0)
 print(visit[n-1][m-1])
